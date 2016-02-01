f9134ad40f01e4b02595a2fe58512d68e7a0ac34
longranger <longranger.406@gmail.com>
2016-01-31 18:21:34 -0800

    examples for windows

---
+++ example.win.user.ex
@@ -0,0 +1,47 @@
+#/AppData/
+#/AppData/Local/
+/AppData/Local/Microsoft/Windows
+/AppData/Local/Mozilla/Firefox/Profiles/aag8viq9.default/cache2
+/AppData/Local/Google/Chrome/User Data/Default/Media Cache
+/AppData/Local/Temp
+/AppData/Local/Apple*
+/AppData/Local/CrashDumps
+/AppData/Local/WebEx
+/AppData/Local/IconCache.db
+/AppData/LocalLow/
+#/AppData/Roaming/
+/AppData/Roaming/Apple Computer
+/Application Data
+/Contacts/
+/Cookies
+#/Desktop/
+#/Documents/
+/Documents/backup
+#/Downloads/
+#/Favorites/
+/Links/
+/Local Settings
+#/Music/
+/My Documents
+/NetHood
+/NTUSER.DAT
+/ntuser.dat.LOG1
+/ntuser.dat.LOG2
+/NTUSER.DAT*
+#/NTUSER.DAT{016888bd-6c6f-11de-8d1d-001e0bcde3ec}.TM.blf
+#/NTUSER.DAT{016888bd-6c6f-11de-8d1d-001e0bcde3ec}.TMContainer00000000000000000001.regtrans-ms
+#/NTUSER.DAT{016888bd-6c6f-11de-8d1d-001e0bcde3ec}.TMContainer00000000000000000002.regtrans-ms
+/ntuser.ini
+/ntuser.pol
+#/Pictures/
+/PrintHood
+/Recent
+/Saved Games/
+/Searches/
+/SendTo
+#/Start Menu
+/Templates
+/Tracing/
+/tst
+#/Videos/
+#/vimfiles/
